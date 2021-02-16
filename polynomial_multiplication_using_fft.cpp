7a3af8978a09b60e67e29ca14e88fa8075289cf9
shubhamxpatel <48165598+shubhamxpatel@users.noreply.github.com>
2021-02-16 23:11:43 +0530

    Update polynomial_multiplication_using_fft.cpp

--- polynomial_multiplication_using_fft.cpp
+++ polynomial_multiplication_using_fft.cpp
@@ -1,3 +1,11 @@
+/*
+In this programme you have to enter polynomials in coefficient form for eg p1=2x^2+x+1 p2=x^3+1 then first enter two numbers m,n where m=degree(p1)+1 n=degree(p2)+1 and then enter m numbers like for p1 1 1 2 and for p2 1 0 0 1
+
+and after entering all data you will get your result like
+
+2x^(5)+1x^(4)+1x^(3)+2x^(2)+1x^(1)+1x^(0)
+*/
+
 #include <bits/stdc++.h> 
 using namespace std; 
 
